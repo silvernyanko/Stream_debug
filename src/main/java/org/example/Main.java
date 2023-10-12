6d679dd381cccefe34fa8cb9e319541a8b61a1f6
silvernyanko <h8octf.jay.mcbz@gmail.com>
2023-10-12 11:25:31 +0900

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = List.of("Moomintroll", "Floren", "Moominpappa", "Moominmamma", "Little My", "Snufkin", "Snork", "Sniff", "Mimble", "Hemulen", "Too-Ticky", "Stinky", "the Groke", "the Hattifatteners");
+        List<String> result = names.stream().sorted().map(String::toUpperCase).filter(name -> name.contains("I")).toList();
+        System.out.println(result);
     }
 }
